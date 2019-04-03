cc2a0a189438ea30b26b7d4c56fb0d9527babe84
Pierre-Marie de Rodat <derodat@adacore.com>
2019-04-03 12:07:12 +0200

    Testsuite: add a new driver to run test program

--- run-tests.py
+++ run-tests.py
@@ -13,10 +13,12 @@ import json
 import os.path
 import pprint
 
-import e3.testsuite
-from e3.testsuite.driver import TestDriver
-from e3.testsuite.result import TestStatus
+from e3.fs import sync_tree
 from e3.os.process import Run
+import e3.testsuite
+import e3.testsuite.driver
+from e3.testsuite.process import check_call
+from e3.testsuite.result import TestStatus
 
 
 TESTSUITE_ROOT = os.path.dirname(os.path.abspath(__file__))
@@ -42,6 +44,49 @@ def canonicalize_json(value):
         raise ValueError('Invalid JSON value: {}'.format(value))
 
 
+class TestDriver(e3.testsuite.driver.TestDriver):
+    """
+    Common code for test drivers.
+    """
+
+    def push_status(self, log, status=TestStatus.FAIL):
+        """
+        Log content to the result, set test status and push the result.
+
+        :param None|str log: Optional content to log.
+        """
+        if log:
+            self.result.log += log
+        self.result.set_status(status)
+        self.push_result()
+        return True
+
+    def push_success(self):
+        """
+        Shortcut to set test status to PASS and to push the result.
+        """
+        return self.push_status(None, TestStatus.PASS)
+
+    def push_for_diff(self, error_label, a, b, fromfile, tofile):
+        """
+        Shortcut to set test status based on whether "a" and "b" are different.
+
+        :param str error_label: Label to include in the log in case of
+            non-empty diff.
+
+        Arguments `a`, `b`, `fromfile` and `tofile` have the same semantics as
+        difflib.unified_diff's corresponding arguments.
+        """
+        diff = list(difflib.unified_diff(
+            a=a, b=b, fromfile=fromfile, tofile=tofile, lineterm=''))
+
+        if diff:
+            self.push_status(
+                '{}:\n{}'.format(error_label, '\n'.join(diff)))
+            raise e3.testsuite.TestAbort
+
+
+
 class DecoderTestDriver(TestDriver):
     """
     Test driver to run the "ada_toml_decode" program.
@@ -60,23 +105,6 @@ class DecoderTestDriver(TestDriver):
 
     def add_test(self, dag):
         self.add_fragment(dag, 'run')
-
-    def push_status(self, log, status=TestStatus.FAIL):
-        """
-        Log content to the result, set test status and push the result.
-
-        :param None|str log: Optional content to log.
-        """
-        if log:
-            self.result.log += log
-        self.result.set_status(status)
-        self.push_result()
-
-    def push_success(self):
-        """
-        Shortcut to set test status to PASS and to push the result.
-        """
-        self.push_status(None, TestStatus.PASS)
 
     def run(self, previous_values):
         test_dir = self.test_env['test_dir']
@@ -132,26 +160,104 @@ class DecoderTestDriver(TestDriver):
             p_output_pretty = pprint.pformat(p_output_json).splitlines()
             expected_pretty = pprint.pformat(expected_json).splitlines()
 
-            diff = difflib.unified_diff(
-                a=p_output_pretty, b=expected_pretty,
-                fromfile='decoder output',
-                tofile='expected output',
-                lineterm='')
+            self.push_for_diff('Unexpected JSON output for the decoder',
+                               p_output_pretty, expected_pretty,
+                               'decoder output', 'expected output')
 
-            return self.push_status(
-                'Unexpected JSON output for the decoder:'
-                '\n{}'.format('\n'.join(diff)))
+        return self.push_success()
+
+
+class RunProgramTestDriver(TestDriver):
+    """
+    Test driver to build and run a program that uses the Ada_TOML project.
+
+    This test driver builds the test-provided "main.adb" program and then run
+    it, checking that its status code is 0 and that its output matches the
+    content of the test-provided "test.out" file.
+    """
+
+    main_file = 'main.adb'
+    expected_output_file = 'test.out'
+
+    def add_test(self, dag):
+        self.add_fragment(dag, 'build')
+        self.add_fragment(dag, 'run', after=['build'])
+
+    def build(self, previous_values):
+        # Copy all test material to the working directory
+        sync_tree(self.test_env['test_dir'], self.test_env['working_dir'])
+
+        # Create a project file to build the test program
+        project_file = os.path.join(self.test_env['working_dir'], 'p.gpr')
+        with open(project_file, 'w') as f:
+            f.write("""
+                with "ada_toml";
+
+                project P is
+                    for Main use ("main.adb");
+                    for Object_Dir use "obj";
+
+                    package Compiler is
+                        for Default_Switches ("Ada") use ("-g");
+                    end Compiler;
+                end P;
+            """)
+
+        # Now build it
+        check_call(self, ['gprbuild', '-P', project_file, '-p'])
+
+        return True
+
+    def run(self, previous_values):
+        if not previous_values['build']:
+            return
+
+        # Run the test program
+        p = check_call(self, ['obj/main'])
+
+        # Check that its output matches expectations
+        with open(os.path.join(self.test_env['test_dir'],
+                               self.expected_output_file), 'r') as f:
+            expected_output = f.read().splitlines()
+
+        p_output = p.out.splitlines()
+
+        self.push_for_diff(
+            'Unexpected test program output',
+            p.out.splitlines(),
+            expected_output,
+            '{} output'.format(self.main_file),
+            self.expected_output_file)
 
         return self.push_success()
 
 
 class Testsuite(e3.testsuite.Testsuite):
     TEST_SUBDIR = 'tests'
-    DRIVERS = {'decoder': DecoderTestDriver}
+    DRIVERS = {'decoder': DecoderTestDriver,
+               'run-program': RunProgramTestDriver}
 
     @property
     def default_driver(self):
         return 'decoder'
+
+    def add_options(self):
+        self.main.argument_parser.add_argument(
+            '--no-auto-path',
+            action='store_true',
+            help='Do not automatically add this repository to'
+                 ' GPR_PROJECT_PATH. Adding it is the default for developer'
+                 ' convenience.')
+
+    def tear_up(self):
+        if not self.main.args.no_auto_path:
+            old_value = os.environ.get('GPR_PROJECT_PATH', '')
+            if old_value:
+                new_value = '{}{}{}'.format(TESTUITE_ROOT, os.path.pathsep,
+                                            old_value)
+            else:
+                new_value = TESTSUITE_ROOT
+            os.environ['GPR_PROJECT_PATH'] = new_value
 
 
 if __name__ == '__main__':
