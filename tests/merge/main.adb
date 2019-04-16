f0eb02cf05197827d0508321c01823e1603f8a27
Pierre-Marie de Rodat <derodat@adacore.com>
2019-04-16 19:55:39 +0200

    tests/merge: enhance testcase to cover the Constraint_Error case

--- tests/merge/main.adb
+++ tests/merge/main.adb
@@ -5,14 +5,29 @@ with TOML;
 procedure Main is
    Table1 : constant TOML.TOML_Value := TOML.Create_Table;
    Table2 : constant TOML.TOML_Value := TOML.Create_Table;
+   Table3 : constant TOML.TOML_Value := TOML.Create_Table;
    Arr    : constant TOML.TOML_Value := TOML.Create_Array;
 begin
    --  Populate tables
    Table1.Set ("a", TOML.Create_Integer (1));
- 
+
    Arr.Append (TOML.Create_Integer (1));
    Table2.Set ("b", Arr);
-   
+
+   Table3.Set ("a", TOML.Create_Integer (3));
+
+   --  Expect a constraint error on duplicate keys
+   declare
+      Dummy : TOML.TOML_Value;
+   begin
+      Dummy := TOML.Merge (Table1, Table3);
+      Ada.Text_IO.Put_Line ("No exception...");
+   exception
+      when Constraint_Error =>
+         Ada.Text_IO.Put_Line
+           ("Merging two tables with duplicate keys raises an exception");
+   end;
+   Ada.Text_IO.New_Line;
 
    declare
       Merged : constant TOML.TOML_Value := TOML.Merge (Table1, Table2);
@@ -20,6 +35,7 @@ begin
       -- Change array value to see the shallow copy modified
       Arr.Append (TOML.Create_Integer (2));
 
+      Ada.Text_IO.Put_Line ("Merged table:");
       Ada.Text_IO.Put_Line (Merged.Dump_As_String);
    end;
 end Main;
