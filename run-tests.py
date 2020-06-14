0aa50453fc3920d1a1ca5090ec18321b54c16ab0
Pierre-Marie de Rodat <pmderodat@kawie.fr>
2020-06-14 12:39:30 +0200

    Switch the testsuite to Python3
    Also fix a very embarrassing bug: the JSON dump comparator never
    complained about expected/actual discrepancies... So this commit also
    reveals lots of errors in the testsuite. Upcoming commits will fix them.

--- run-tests.py
+++ run-tests.py
@@ -12,79 +12,46 @@ import difflib
 import json
 import os.path
 import pprint
+import subprocess
 import sys
 
 from e3.fs import sync_tree
 from e3.os.process import Run
-import e3.testsuite
-import e3.testsuite.driver
-from e3.testsuite.process import check_call
-from e3.testsuite.result import TestStatus
+from e3.testsuite import Testsuite
+from e3.testsuite.driver.classic import (
+    ClassicTestDriver, TestAbortWithError, TestAbortWithFailure)
+from e3.testsuite.result import Log, binary_repr
 
 
-TESTSUITE_ROOT = os.path.dirname(os.path.abspath(__file__))
-
-
-def canonicalize_json(value):
-    """
-    Decode all bytes in the ``value`` JSON value.
-
-    This clones the input JSON value, replacing all bytes with the
-    corresponding strings, assuming UTF-8 encoding.
-    """
-    if isinstance(value, (int, bool, unicode)):
-        return value
-    elif isinstance(value, str):
-        return value.decode('utf-8')
-    elif isinstance(value, list):
-        return [canonicalize_json(item) for item in value]
-    elif isinstance(value, dict):
-        return {canonicalize_json(key): canonicalize_json(item)
-                for key, item in value.iteritems()}
-    else:
-        raise ValueError('Invalid JSON value: {}'.format(repr(value)))
-
-
-class TestDriver(e3.testsuite.driver.TestDriver):
+class TestDriver(ClassicTestDriver):
     """
     Common code for test drivers.
     """
 
-    def push_status(self, log, status=TestStatus.FAIL):
+    def fail_if_diff(self, error_label, expected, actual, expected_file,
+                     actual_file):
         """
-        Log content to the result, set test status and push the result.
-
-        :param None|str log: Optional content to log.
-        """
-        if log:
-            self.result.log += log
-        self.result.set_status(status)
-        self.push_result()
-        return True
-
-    def push_success(self):
-        """
-        Shortcut to set test status to PASS and to push the result.
-        """
-        return self.push_status(None, TestStatus.PASS)
-
-    def push_for_diff(self, error_label, a, b, fromfile, tofile):
-        """
-        Shortcut to set test status based on whether "a" and "b" are different.
+        Shortcut to stop the test with a failure if "expected" and "actual" are
+        different.
 
         :param str error_label: Label to include in the log in case of
             non-empty diff.
-
-        Arguments `a`, `b`, `fromfile` and `tofile` have the same semantics as
-        difflib.unified_diff's corresponding arguments.
+        :param list[str] expected: List of expected lines.
+        :param list[str] actual: List of actual lines.
+        :param str expected_file: Name of the file for expected lines. This is
+            for display purposes only: the file name does not need to exist.
+        :param str actual_file: Like "expected_file", but for actual lines.
         """
         diff = list(difflib.unified_diff(
-            a=a, b=b, fromfile=fromfile, tofile=tofile, lineterm=''))
+            a=expected, b=actual, fromfile=expected_file, tofile=actual_file,
+            lineterm=''))
 
         if diff:
-            self.push_status(
-                '{}:\n{}'.format(error_label, '\n'.join(diff)))
-            raise e3.testsuite.TestAbort
+            self.result.expected = Log('\n'.join(expected))
+            self.result.out = Log('\n'.join(actual))
+            self.result.diff = Log('\n'.join(diff))
+            self.result.log += 'Diff:\n{}\n'.format(self.result.diff.log)
+            raise TestAbortWithFailure(error_label)
 
 
 
@@ -106,25 +73,139 @@ class DecoderTestDriver(TestDriver):
     encoder_program = 'obj-checkers/ada_toml_encode'
     input_file = 'input.toml'
 
-    def add_test(self, dag):
-        self.add_fragment(dag, 'run')
+    def _run_subprocess(self, args, input_file):
+        self.result.log += "Running: {}\n".format(" ".join(args))
+        result = subprocess.run(
+            args,
+            stdin=input_file, stdout=subprocess.PIPE, stderr=subprocess.STDOUT
+        )
+        self.result.log += "Status code: {}\n".format(result.returncode)
+        try:
+            result.decoded_stdout = result.stdout.decode('utf-8')
+        except UnicodeDecodeError as exc:
+            self.result.log += "Output:\n{}\n".format(
+                binary_repr(result.stdout))
+            raise TestAbortWithFailure(
+                "Cannot decode UTF-8 output: {}".format(exc))
+        self.result.log += "Output:\n{}\n".format(result.decoded_stdout)
+        return result
 
     def _run_decoder(self, filename):
         with open(filename, 'rb') as f:
-            return Run([os.path.join(TESTSUITE_ROOT, self.decoder_program)],
-                       input=f)
+            return self._run_subprocess(
+                [os.path.join(self.env.root_dir, self.decoder_program)], f,
+            )
 
     def _run_encoder(self, filename):
         with open(filename, 'rb') as f:
-            return Run([os.path.join(TESTSUITE_ROOT, self.encoder_program)],
-                       input=f)
+            return self._run_subprocess(
+                [os.path.join(self.env.root_dir, self.encoder_program)], f,
+            )
 
-    def run(self, previous_values):
-        work_dir = self.test_env['working_dir']
+    def fail_if_json_mismatch(
+        self, error_label, expected, actual, expected_file, actual_file
+    ):
+        """
+        Compare two JSON dumps and stop the test with a failure if "a" and "b"
+        don't match.
 
-        # Copy all test material to the working directory
-        sync_tree(self.test_env['test_dir'], work_dir)
+        See "fail_if_diff" for arguments semantic. The only difference is that
+        expected and actual are JSON objects.
+        """
+        def canonicalize(value):
+            if isinstance(value, list):
+                return {'type': 'array', 'value': value}
+            elif isinstance(value, dict) and set(value) != {'type', 'value'}:
+                return {'type': 'dict', 'value': value}
+            else:
+                return value
 
+        def is_valid(value):
+            if (
+                not isinstance(value, dict)
+                or set(value) != {'type', 'value'}
+            ):
+                return False
+            t, v = value['type'], value['value']
+            return (
+                (t == 'dict' and isinstance(v, dict))
+                or (t == 'array' and isinstance(v, list))
+                or (t in ('string', 'float', 'integer', 'bool', 'datetime',
+                          'local-datetime', 'date', 'time')
+                    and isinstance(v, str))
+            )
+
+        def helper(path, expected, actual):
+            path_label = 'root' if path is None else path
+            path_prefix = path or ''
+            expected = canonicalize(expected)
+            actual = canonicalize(actual)
+            if not is_valid(expected):
+                raise TestAbortWithError(
+                    "expected:{}: invalid object description"
+                    .format(path_label)
+                )
+            if not is_valid(actual):
+                raise TestAbortWithFailure(
+                    "actual:{}: invalid object description".format(path_label)
+                )
+
+            def error(label):
+                self.fail_if_diff(
+                    '{}: {}'.format(path_label, label),
+                    pprint.pformat(expected).splitlines(),
+                    pprint.pformat(actual).splitlines(),
+                    expected_file,
+                    actual_file
+                )
+
+            t = expected['type']
+            exp_v = expected['value']
+            act_v = actual['value']
+            if t != actual['type']:
+                error('type mismatch')
+            elif t == 'dict':
+                # First check keys equivalence, then check the inner values
+                if set(exp_v) != set(act_v):
+                    error('unexpected dict keys')
+                for k in sorted(exp_v):
+                    helper('{}[{}]'.format(path_label, repr(k)),
+                           exp_v[k], act_v[k])
+            elif t == 'array':
+                # First check array length, then check the inner values
+                if len(exp_v) != len(act_v):
+                    error('unexpected array length')
+                for i, (e, a) in enumerate(zip(exp_v, act_v)):
+                    helper('{}[{}]'.format(path_label, i), e, a)
+            elif t == 'float':
+                # First, check "special values"
+                if (
+                    (exp_v in ('nan', '+nan')
+                     and act_v not in ('nan', '+nan'))
+                    or (exp_v == '-nan' and act_v != '-nan')
+                    or (exp_v in ('inf', '+inf')
+                        and act_v not in ('inf', '+inf'))
+                    or (exp_v == '-inf' and act_v != '-inf')
+                ):
+                    error('value mismatch')
+
+                # We cannot expect exact string representation matches for
+                # floats. Just try to make sure both designated values are
+                # "almost equal": check the precision for 10 decimals
+                # (arbitrary, but deemed good enough for now).
+                e = float(exp_v)
+                a = float(act_v)
+                abs_e = abs(e)
+                abs_a = abs(a)
+                epsilon = max(abs_a, abs_e) / 10**10
+                if a < e - epsilon or a > e + epsilon:
+                    error('value mismatch')
+            elif exp_v != act_v:
+                error('value mismatch')
+
+        helper(None, expected, actual)
+
+    def run(self):
         # Get the expected JSON document, or the expected error message
         expected_error = None
         try:
@@ -132,95 +213,82 @@ class DecoderTestDriver(TestDriver):
         except KeyError:
             expected_json = None
             expected_error = self.test_env['error']
-        else:
-            # The object decoded from test.yaml contains sometimes strings,
-            # sometimes unicode objects. Canonicalize to Unicode to have the
-            # same objects as from the JSON output.
-            expected_json = canonicalize_json(expected_json)
-
-        # Read the input TOML content
-        with open(os.path.join(work_dir, self.input_file), 'rb') as f:
-            input_str = f.read()
 
         # Run the decoder with the TOML content on the standard input
-        p = self._run_decoder(os.path.join(work_dir, self.input_file))
+        p = self._run_decoder(self.working_dir(self.input_file))
 
         # If we expected an error, make sure we have the expected one
         if expected_error:
-            if p.status == 0:
-                return self.push_status(
+            out = p.stdout.decode('utf-8')
+            if p.returncode == 0:
+                raise TestAbortWithFailure(
                     'Error expected, but parsing succeeded')
             elif (
-                expected_error is not True and
-                p.out.strip() != expected_error
+                expected_error is not True
+                and out.strip() != expected_error
             ):
-                return self.push_status('Unexpected error:\n{}'.format(p.out))
+                raise TestAbortWithFailure('Unexpected error')
             else:
-                return self.push_success()
+                return
 
         # Otherwise, make sure the decoder succeeded and produced the expected
         # result.
-        if p.status != 0:
-            return self.push_status(
-                'Decoder exitted with error status ({}):'
-                '\n{}'.format(p.status, p.out))
+        if p.returncode != 0:
+            raise TestAbortWithFailure(
+                'Decoder exitted with error status ({})'.format(p.returncode))
 
-        json_text_output = p.out
-
+        json_text_output = p.decoded_stdout
         try:
-            p_output_json = json.loads(p.out)
-        except ValueError as exc:
-            return self.push_status(
-                'Cannot parse the output JSON document:'
-                '\n{}'
-                '\nOutput was: {}'.format(exc, p.out))
+            p_output_json = json.loads(json_text_output)
+        except (UnicodeDecodeError, ValueError) as exc:
+            raise TestAbortWithFailure('Cannot parse the output JSON document')
 
-        # Canonicalize to Unicode the output of `json.loads` for the same
-        # reason we canonicalized `expected_json`.
-        p_output_json = canonicalize_json(expected_json)
-
-        if p_output_json != expected_json:
-            p_output_pretty = pprint.pformat(p_output_json).splitlines()
-            expected_pretty = pprint.pformat(expected_json).splitlines()
-
-            self.push_for_diff('Unexpected JSON output for the decoder',
-                               p_output_pretty, expected_pretty,
-                               'decoder output', 'expected output')
+        self.fail_if_json_mismatch(
+            'Unexpected JSON output for the decoder',
+            expected_json,
+            p_output_json,
+            'expected output',
+            'decode output',
+        )
 
         # Now, try to reformat a TOML document from the JSON output and make
         # sure it produces something that the decoder can re-parse the same
         # way.
 
         # Put the JSON input in a file (to ease post-mortem analysis)
-        input_json_file = os.path.join(work_dir, 'input.json')
-        with open(input_json_file, 'w') as f:
-            f.write(json_text_output)
+        input_json_file = self.working_dir('input.json')
+        with open(input_json_file, 'wb') as f:
+            f.write(p.stdout)
 
         # Run the encoder on it
         p = self._run_encoder(input_json_file)
-        if p.status != 0:
-            return self.push_status('Encoder failed: {}'.format(p.out))
+        if p.returncode != 0:
+            raise TestAbortWithFailure('Encoder failed: {}'.format(p.stdout))
 
         # Put the resulting TOML document in a file
-        input_toml_file = os.path.join(work_dir, 'second-input.toml')
-        with open(input_toml_file, 'w') as f:
-            f.write(p.out)
+        input_toml_file = self.working_dir('second-input.toml')
+        with open(input_toml_file, 'wb') as f:
+            f.write(p.stdout)
 
         # Run the decoder on it
         p = self._run_decoder(input_toml_file)
-        if p.status != 0:
-            return self.push_status(
-                'Second decoder exitted with error status ({}):'
-                '\n{}'.format(p.status, p.out))
+        if p.returncode != 0:
+            raise TestAbortWithFailure(
+                'Second decoder exitted with error status ({})'
+                .format(p.returncode)
+            )
 
-        if json_text_output != p.out:
-            self.push_for_diff('Unexpected second decoder output',
-                               p.out.splitlines(),
-                               json_text_output.splitlines(),
-                               'second decoder output',
-                               'first decoder output')
-
-        return self.push_success()
+        try:
+            new_json_text_output = p.decoded_stdout
+        except UnicodeDecodeError as exc:
+            raise TestAbortWithFailure(
+                'Cannot decode the output JSON document: {}'.format(exc))
+        if json_text_output != new_json_text_output:
+            self.fail_if_diff('Unexpected second decoder output',
+                              p.out.splitlines(),
+                              json_text_output.splitlines(),
+                              'second decoder output',
+                              'first decoder output')
 
 
 class RunProgramTestDriver(TestDriver):
@@ -235,16 +303,9 @@ class RunProgramTestDriver(TestDriver):
     main_file = 'main.adb'
     expected_output_file = 'test.out'
 
-    def add_test(self, dag):
-        self.add_fragment(dag, 'build')
-        self.add_fragment(dag, 'run', after=['build'])
-
-    def build(self, previous_values):
-        # Copy all test material to the working directory
-        sync_tree(self.test_env['test_dir'], self.test_env['working_dir'])
-
-        # Create a project file to build the test program
-        project_file = os.path.join(self.test_env['working_dir'], 'p.gpr')
+    def run(self):
+        # Create a project file to build the test program and build it
+        project_file = self.working_dir('p.gpr')
         with open(project_file, 'w') as f:
             f.write("""
                 with "ada_toml";
@@ -258,47 +319,36 @@ class RunProgramTestDriver(TestDriver):
                     end Compiler;
                 end P;
             """)
-
-        # Now build it
-        check_call(self, ['gprbuild', '-P', project_file, '-p'])
-
-        return True
-
-    def run(self, previous_values):
-        if not previous_values['build']:
-            return
+        self.shell(['gprbuild', '-P', project_file, '-p'])
 
         # Run the test program
-        p = check_call(self, ['obj/main'])
+        p = self.shell(['obj/main'])
 
         # Check that its output matches expectations
-        with open(os.path.join(self.test_env['test_dir'],
-                               self.expected_output_file), 'r') as f:
+        with open(self.test_dir(self.expected_output_file), 'r') as f:
             expected_output = f.read().splitlines()
 
         p_output = p.out.splitlines()
 
-        self.push_for_diff(
+        self.fail_if_diff(
             'Unexpected test program output',
             p.out.splitlines(),
             expected_output,
             '{} output'.format(self.main_file),
             self.expected_output_file)
 
-        return self.push_success()
 
-
-class Testsuite(e3.testsuite.Testsuite):
-    TEST_SUBDIR = 'tests'
-    DRIVERS = {'decoder': DecoderTestDriver,
-               'run-program': RunProgramTestDriver}
+class ADATomlTestsuite(Testsuite):
+    tests_subdir = 'tests'
+    test_driver_map = {'decoder': DecoderTestDriver,
+                       'run-program': RunProgramTestDriver}
 
     @property
     def default_driver(self):
         return 'decoder'
 
-    def add_options(self):
-        self.main.argument_parser.add_argument(
+    def add_options(self, parser):
+        parser.add_argument(
             '--no-auto-path',
             action='store_true',
             help='Do not automatically add this repository to'
@@ -309,10 +359,10 @@ class Testsuite(e3.testsuite.Testsuite):
         if not self.main.args.no_auto_path:
             old_value = os.environ.get('GPR_PROJECT_PATH', '')
             if old_value:
-                new_value = '{}{}{}'.format(TESTSUITE_ROOT, os.path.pathsep,
+                new_value = '{}{}{}'.format(self.root_dir, os.path.pathsep,
                                             old_value)
             else:
-                new_value = TESTSUITE_ROOT
+                new_value = self.root_dir
             os.environ['GPR_PROJECT_PATH'] = new_value
 
 
@@ -320,12 +370,5 @@ if __name__ == '__main__':
     # Some test YAML documents create big recursions in the YAML parser
     sys.setrecursionlimit(10000)
 
-    suite = Testsuite(os.path.dirname(__file__))
-    suite.testsuite_main()
-
-    # Display statistics about test results: number of tests per status
-    stats = [(str(name).split('.')[1], count)
-             for name, count in suite.test_status_counters.items()
-             if count]
-    for name, count in sorted(stats):
-        print('{: <8} {}'.format(name + ':', count))
+    suite = ADATomlTestsuite()
+    sys.exit(suite.testsuite_main())
